6c864b128125696ea7e834614cd4bedd6fc0b573
Nikita Kryazhin <kryzhin94@gmail.com>
2022-09-17 12:22:34 +0200

    module2-task1: added generate-command for CLI

--- src/common/file-reader/tsv-file-reader.ts
+++ src/common/file-reader/tsv-file-reader.ts
@@ -1,80 +1,34 @@
-import { readFileSync } from 'fs';
-import { OfferType } from '../../types/offer-type.enum.js';
-import { Facilities } from '../../types/facilities.enum.js';
-import { Offer } from '../../types/offer.type.js';
+import EventEmitter from 'events';
+import { createReadStream } from 'fs';
 import { FileReaderInterface } from './file-reader.interface.js';
-import { CityType } from '../../types/city-type.enum.js';
 
-export default class TSVFileReader implements FileReaderInterface {
-  private rawData = '';
-
-  constructor(public filename: string) { }
-
-  read(): void {
-    this.rawData = readFileSync(this.filename, { encoding: 'utf8' });
+export default class TSVFileReader extends EventEmitter implements FileReaderInterface {
+  constructor(public filename: string) {
+    super();
   }
 
-  toArray(): Offer[] {
-    if (!this.rawData) {
-      return [];
+  async read():Promise<void> {
+    const stream = createReadStream(this.filename, {
+      highWaterMark: 16384, // 16KB
+      encoding: 'utf-8',
+    });
+
+    let lineRead = '';
+    let endLinePosition = -1;
+    let importedRowCount = 0;
+
+    for await (const chunk of stream) {
+      lineRead += chunk.toString();
+
+      while ((endLinePosition = lineRead.indexOf('\n')) >= 0) {
+        const completeRow = lineRead.slice(0, endLinePosition + 1);
+        lineRead = lineRead.slice(++endLinePosition);
+        importedRowCount++;
+
+        this.emit('line', completeRow);
+      }
     }
 
-    return this.rawData
-      .split('\n')
-      .filter((row) => row.trim() !== '')
-      .map((line) => line.split('\t'))
-      .map((test) => {
-        const [
-          title,
-          description,
-          createdDate,
-          city,
-          previewPath,
-          images,
-          isPremium,
-          rating,
-          type,
-          bedroomsCount,
-          guestsCount,
-          price,
-          facilities,
-          commentsCount,
-          latitude,
-          longitude,
-          name,
-          email,
-          avatarPath,
-          password,
-          isPro
-        ] = test;
-        console.log(test);
-        return {
-          title,
-          description,
-          createdDate: new Date(createdDate),
-          city: CityType[city as 'Paris' | 'Cologne' | 'Brussels' | 'Amsterdam' | 'Hamburg' | 'Dusseldorf'],
-          previewPath,
-          images: images.split(';'),
-          isPremium: Boolean(isPremium),
-          rating: Number.parseFloat(rating),
-          type: OfferType[type as 'Apartment' | 'House' | 'Room' | 'Hotel'],
-          bedroomsCount: Number.parseInt(bedroomsCount, 10),
-          guestsCount: Number.parseInt(guestsCount, 10),
-          price: Number.parseInt(price, 10),
-          facilities: facilities.split(';').map((facility) => Facilities[facility as 'Breakfast' | 'AirConditioning' | 'Workspace' | 'BabySeat' | 'Washer' | 'Towels' | 'Fridge' ]),
-          commentsCount: Number.parseInt(commentsCount, 10),
-          location: {
-            latitude: Number.parseFloat(latitude),
-            longitude: Number.parseFloat(longitude)
-          },
-          author: {
-            name,
-            email,
-            avatarPath,
-            password,
-            isPro: Boolean(isPro)
-          },
-        };
-      });
+    this.emit('end', importedRowCount);
   }
 }
---
+++ src/types/mock-data.type.ts
@@ -0,0 +1,9 @@
+export type MockData = {
+  titles: string[];
+  descriptions: string[];
+  previewImagesPath: string[];
+  images: string[];
+  users: string[];
+  emails: string[];
+  avatars: string[];
+};
